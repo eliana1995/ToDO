f80e85072e62556265b44e4b3b0b2c2e211dbe94
eliana <113516213+eliana1995@users.noreply.github.com>
2023-01-30 17:20:58 +0100

    Add files via upload

---
+++ CodingSimplified ToDo project/CodingSimplified ToDo App/Data/Models/TaskModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CodingSimplified_ToDo_App.Data.Models
+{
+    public class TaskModel 
+    {
+        public string TaskName  { get; set; }
+    public bool IsComplete{ get; set; }
+}
+}
+
